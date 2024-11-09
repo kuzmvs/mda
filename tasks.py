f0a9e45a8ac0b476f92bcdf7ba586f97c36c86c0
Владислав Кузьменко <vkuzmenko@inno.tech>
2024-11-09 18:12:17 +0300

    refactoring hw1_task_2

--- tasks.py
+++ tasks.py
@@ -1,3 +1,19 @@
+# Задание 1
+# Напишите функцию, возвращающую округленную взвешенную сумму оценок по данным оценкам и весам.
+# Можете посчитать свою оценку за курс :)
+#
+# %%
+import numpy as np
+
+
+def result_mark(weights, marks):
+    return sum(w * m for w, m in zip(weights, marks))
+
+
+weights = np.array([0.35, 0.4, 0.15, 0.1])
+marks = np.array([6, 9, 8, 7])
+result_mark(weights, marks)
+
 # Задание 2
 # Напишите функцию, меняющую каждое третье (начиная с 0) значение массива целых чисел на заданное число.
 # Например, если на вход поступает массив `array([3, 5, 1, 0, -3, 22, 213436])`
@@ -8,9 +24,8 @@ import numpy as np
 
 
 def change_array(array, number):
-    for i in range(0, array.size):
-        if i == 0 or (i + 3) % 3 == 0:
-            array[i] = number
+    # from start to end, exclusive, set every 3nd element to "number"
+    array[::3] = number
 
 
 array = np.array([3, 5, 1, 0, -3, 22, 213436])
